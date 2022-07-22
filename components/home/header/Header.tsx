6a8d69c33e62c8b62832dc07f2466436f0a9c20f
unknown <rayhan.senbag.bd@gmail.com>
2022-07-22 22:30:27 +0600

    df

--- components/home/header/Header.tsx
+++ components/home/header/Header.tsx
@@ -5,6 +5,7 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import GridViewIcon from "@mui/icons-material/GridView";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SearchIcon from "@mui/icons-material/Search";
+import SortIcon from "@mui/icons-material/Sort";
 import { Button, Input } from "@mui/material";
 
 const Header = () => {
@@ -22,72 +23,85 @@ const Header = () => {
 
   return (
     <>
-      <section className='header-container'>
-        {/* logo */}
-        <div className='lg:w-[60%]'>
-          <Image width={150} height={33} src='/logo.png' alt='Logo' />
-        </div>
+      <div className='bg-white lg:border-b-2 md:w-[500px] lg:w-full mx-auto'>
+        <section className='header-container'>
+          {/* logo */}
+          <div className='lg:w-[60%]'>
+            <Image width={150} height={33} src='/logo.png' alt='Logo' />
+          </div>
 
-        {/* main menu */}
-        <div className='main-menu'>
-          {mainMenus.map((menu, index) => (
-            <button
-              onClick={() => setHighlightBtn(index)}
-              className={highlightBtn === index ? "btn-highlight" : ""}
-              key={index}
-            >
-              {menu}
-            </button>
-          ))}
-        </div>
+          <div className='flex justify-between'>
+            {/* main menu */}
+            <div className='main-menu'>
+              {mainMenus.map((menu, index) => (
+                <button
+                  onClick={() => setHighlightBtn(index)}
+                  className={highlightBtn === index ? "btn-highlight" : ""}
+                  key={index}
+                >
+                  {menu}
+                </button>
+              ))}
+            </div>
 
-        {/* user menu */}
-        <div className='user-menus'>
-          <button className='language-btn' onClick={handleLanguage}>
-            {language}
-          </button>
-          <button className='notification-btn'>
-            <MailIcon fontSize='small' />
-            <span className='notification'>14</span>
-          </button>
-          <Button className='add-post-btn' size='small'>
-            post ad
-          </Button>
-          <button className='auth-btn'>
-            <ArrowDropDownIcon />
-          </button>
-        </div>
-
-        <hr className='col-span-4 hidden lg:block' />
-
-        {/* second menu  */}
-        <section className='header-second-menu'>
-          {secondMenus.map((menu, index) => (
-            <Button size='small' key={index}>
-              {menu}
-            </Button>
-          ))}
+            {/* user menu */}
+            <div className='user-menus'>
+              <button className='language-btn' onClick={handleLanguage}>
+                {language}
+              </button>
+              <button className='notification-btn'>
+                <MailIcon fontSize='small' />
+                <span className='notification'>14</span>
+              </button>
+              <Button className='add-post-btn' size='small'>
+                post ad
+              </Button>
+              <button className='auth-btn'>
+                <ArrowDropDownIcon />
+              </button>
+            </div>
+          </div>
         </section>
-      </section>
+      </div>
+
+      {/* second menu  */}
+      <div className='bg-white border-b-2 hidden lg:block'>
+        <section className='header-second-menu'>
+          <div className='hidden lg:block'></div>
+          <div>
+            {secondMenus.map((menu, index) => (
+              <Button key={index}>{menu}</Button>
+            ))}
+          </div>
+          <div className='hidden lg:block'></div>
+        </section>
+      </div>
 
       {/* header third menu  */}
       <section className='header-third-menu'>
-        <div>
-          <GridViewIcon /> Category
-        </div>
-        <div>
-          <LocationOnIcon /> Location
-        </div>
-        <div className='search-btn'>
-          <p
-            className='cursor-pointer'
-            onClick={() => setShowSearchBtn(!showSearchBtn)}
-          >
-            <SearchIcon />
-            {!showSearchBtn && <span>Search</span>}
-          </p>
-          {showSearchBtn && <Input color='warning' placeholder='Search' />}
-        </div>
+        <div className='hidden lg:block'></div>
+        <main>
+          <div>
+            <GridViewIcon /> Categories
+          </div>
+          <div className="md:ml-24 lg:ml-0">
+            <LocationOnIcon /> Locations
+          </div>
+          <div className='search-btn'>
+            <p
+              className='cursor-pointer'
+              onClick={() => setShowSearchBtn(!showSearchBtn)}
+            >
+              <SearchIcon />
+              {!showSearchBtn && <span>Search</span>}
+            </p>
+            {showSearchBtn && <Input color='warning' placeholder='Search' />}
+          </div>
+          <div className='lg:hidden ml-auto'>
+            <SortIcon />
+          </div>
+        </main>
+        <div className='hidden lg:block'></div>
       </section>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
       colors: {
         primary: "#ff6903",
       },
+      gridTemplateColumns: {
+        header: "250px 710px",
+        primary: "250px 500px 160px",
+      },
+      gap: {
+        primary: "50px",
+      },
+      width: {
+        primary: "1010px",
+      },
     },
   },
   plugins: [],
